c1396d257e920ed906a726c8e60e2231029f042d
Argha Kamal Samanta <arghakamal25@gmail.com>
2025-01-08 11:07:53 +0530

    Update llm_response.py

--- llm_response.py
+++ llm_response.py
@@ -50,7 +50,7 @@ def generate_response(query):
     completion = client.chat.completions.create(
         model="mistralai/Mistral-7B-Instruct-v0.3", 
     	messages=messages, 
-    	max_tokens=500
+    	max_tokens=1000
     )
 
     return completion.choices[0].message.content
